14c5205e4f0dae459e309fa52d0969d0ee5cb539
Giuseppe <galardigiuseppe11@gmail.com>
2025-10-13 22:33:12 +0200

    Started prototyping runner module, Added deployment diagram, Modified project structure

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,16 +1,17 @@
-#!/bin/bash -l
-
-#SBATCH --time=00:20:00
-#SBATCH --qos=default
-#SBATCH --partition=gpu
-#SBATCH --account=p200981
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=1
-
-module add Apptainer
-
-# Run the processing
-apptainer pull docker://ollama/ollama
-apptainer exec --nv ollama_latest.sif ollama serve
-apptainer exec --nv ollama_latest.sif ollama pull mistral
+# TODO
+# #!/bin/bash -l
+#
+# #SBATCH --time=00:20:00
+# #SBATCH --qos=default
+# #SBATCH --partition=gpu
+# #SBATCH --account=p200981
+# #SBATCH --nodes=1
+# #SBATCH --ntasks=1
+# #SBATCH --ntasks-per-node=1
+#
+# module add Apptainer
+#
+# # Run the processing
+# apptainer pull docker://ollama/ollama
+# apptainer exec --nv ollama_latest.sif ollama serve
+# apptainer exec --nv ollama_latest.sif ollama pull mistral
