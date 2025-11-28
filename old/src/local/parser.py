afb752bc9c69f532b587d7236150562dc7ca3939
Giuseppe <galardigiuseppe11@gmail.com>
2025-11-28 21:24:58 +0100

    Implemented communcicator abstract class and SSH communicator

--- old/src/local/parser.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-# TODO: validate file
-def parse(path):
-    """
-    This function parses and validates the provided .yml file
-    """
-    with open(path, "r") as file:
-        return yaml.safe_load(file)
